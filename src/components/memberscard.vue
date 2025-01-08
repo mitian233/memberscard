7f689ec452dedb8f51e996500d80f9fadffd9129
mitian233 <mitian233@yahoo.co.jp>
2025-01-08 16:31:39 +0800

    style: update layout for member card component

--- src/components/memberscard.vue
+++ src/components/memberscard.vue
@@ -125,7 +125,7 @@ onBeforeUnmount(() => {
 
 <template>
     <div class="container mx-auto">
-        <div class="w-full overflow-hidden">
+        <div class="w-full flex justify-center items-center">
             <div
                 ref="memberCardRef"
                 class="memberCard w-full h-full max-w-[1000px] max-h-[680px] overflow-hidden bg-black p-5"
