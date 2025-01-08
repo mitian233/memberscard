0572c8e4a0b10aee21246f994ffeb9b92989c278
mitian233 <mitian233@yahoo.co.jp>
2025-01-08 17:05:00 +0800

    Changes to be committed:     modified:   src/components/memberscard.vue

--- src/components/memberscard.vue
+++ src/components/memberscard.vue
@@ -121,6 +121,97 @@ onMounted(() => {
 onBeforeUnmount(() => {
     window.removeEventListener('resize', handleResize);
 });
+
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
+
+const solutions = [
+    {
+        name: 'Insights',
+        description: 'Measure actions your users take',
+        href: '##',
+        icon: `
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+        <path
+          d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
+          stroke="#FB923C"
+          stroke-width="2"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
+          stroke="#FDBA74"
+          stroke-width="2"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
+          stroke="#FDBA74"
+          stroke-width="2"
+        />
+      </svg>
+    `
+    },
+    {
+        name: 'Automations',
+        description: 'Create your own targeted content',
+        href: '##',
+        icon: `
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+        <path
+          d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
+          stroke="#FB923C"
+          stroke-width="2"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
+          stroke="#FDBA74"
+          stroke-width="2"
+        />
+      </svg>
+    `
+    },
+    {
+        name: 'Reports',
+        description: 'Keep track of your growth',
+        href: '##',
+        icon: `
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+        <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
+        <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
+        <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
+        <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
+        <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
+        <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
+      </svg>
+    `
+    }
+];
 </script>
 
 <template>
@@ -398,113 +489,141 @@ onBeforeUnmount(() => {
             <button class="btn" @click="saveAsImage">保存图片</button>
             <button class="btn" @click="copyImage">复制图片</button>
         </div>
-        <table class="table-out">
-            <tbody>
-                <tr>
-                    <th>名称</th>
-                    <th>
-                        <input
-                            class="input input-bordered w-full max-w-xs"
-                            placeholder="名称"
-                            v-model="name"
-                        />
-                    </th>
-                </tr>
-                <tr>
-                    <th>名称英文</th>
-                    <th>
-                        <input
-                            class="input input-bordered w-full max-w-xs"
-                            placeholder="Name"
-                            v-model="nameEn"
-                        />
-                    </th>
-                </tr>
-                <tr>
-                    <th>乐队名称</th>
-                    <th>
-                        <input
-                            class="input input-bordered w-full max-w-xs"
-                            placeholder="Morfonica"
-                            v-model="bandName"
-                        />
-                    </th>
-                </tr>
-                <tr>
-                    <th>生日</th>
-                    <th>
-                        <input
-                            class="input input-bordered w-full max-w-xs"
-                            placeholder="9/15"
-                            v-model="birthday"
-                        />
-                    </th>
-                </tr>
-                <tr>
-                    <th>调整图像</th>
-                    <th>
-                        <table class="table-in">
-                            <tbody>
-                                <tr>
-                                    <th>加载</th>
-                                    <th>
-                                        <input
-                                            class="file-input w-full max-w-xs"
-                                            type="file"
-                                            accept="image/*"
-                                            @change="handleFileChange"
-                                        />
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th>缩放</th>
-                                    <th>
-                                        <input
-                                            type="range"
-                                            min="1"
-                                            max="300"
-                                            class="range"
-                                            v-model="AvatarScaleStr"
-                                        />
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th>x</th>
-                                    <th>
-                                        <input
-                                            type="range"
-                                            min="0"
-                                            :max="AvatarImgObj.width - 360"
-                                            class="range"
-                                            v-model="AvatarImgOffsetStr.x"
-                                        />
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th>y</th>
-                                    <th>
-                                        <input
-                                            type="range"
-                                            min="0"
-                                            :max="AvatarImgObj.height - 550"
-                                            class="range"
-                                            v-model="AvatarImgOffsetStr.y"
-                                        />
-                                    </th>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </th>
-                </tr>
-            </tbody>
-        </table>
+    </div>
+    <div class="fixed bottom-10 w-full max-w-sm left-1/2 -translate-x-1/2">
+        <Popover v-slot="{ open }" class="relative flex justify-center">
+            <PopoverButton
+                :class="open ? 'text-white' : 'text-white/90'"
+                class="group inline-flex items-center rounded-md bg-orange-700 px-5 py-2 text-base font-medium hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+            >
+                <span>自定义内容</span>
+            </PopoverButton>
+
+            <transition
+                enter-active-class="transition duration-200 ease-out"
+                enter-from-class="translate-y-1 opacity-0"
+                enter-to-class="translate-y-0 opacity-100"
+                leave-active-class="transition duration-150 ease-in"
+                leave-from-class="translate-y-0 opacity-100"
+                leave-to-class="translate-y-1 opacity-0"
+            >
+                <PopoverPanel
+                    class="absolute left-1/2 bottom-1/2 z-10 mt-3 w-screen max-w-lg -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl"
+                >
+                    <div
+                        class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5 hover:opacity-90 duration-200"
+                    >
+                        <div class="bg-gray-50 p-4">
+                            <table class="table-out">
+                                <tbody>
+                                    <tr>
+                                        <th>名称</th>
+                                        <th>
+                                            <input
+                                                class="input input-bordered w-full max-w-xs"
+                                                placeholder="名称"
+                                                v-model="name"
+                                            />
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th>名称英文</th>
+                                        <th>
+                                            <input
+                                                class="input input-bordered w-full max-w-xs"
+                                                placeholder="Name"
+                                                v-model="nameEn"
+                                            />
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th>乐队名称</th>
+                                        <th>
+                                            <input
+                                                class="input input-bordered w-full max-w-xs"
+                                                placeholder="Morfonica"
+                                                v-model="bandName"
+                                            />
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th>生日</th>
+                                        <th>
+                                            <input
+                                                class="input input-bordered w-full max-w-xs"
+                                                placeholder="9/15"
+                                                v-model="birthday"
+                                            />
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th>调整图像</th>
+                                        <th>
+                                            <table class="table-in">
+                                                <tbody>
+                                                    <tr>
+                                                        <th>加载</th>
+                                                        <th>
+                                                            <input
+                                                                class="file-input w-full max-w-xs"
+                                                                type="file"
+                                                                accept="image/*"
+                                                                @change="handleFileChange"
+                                                            />
+                                                        </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>缩放</th>
+                                                        <th>
+                                                            <input
+                                                                type="range"
+                                                                min="1"
+                                                                max="300"
+                                                                class="range"
+                                                                v-model="AvatarScaleStr"
+                                                            />
+                                                        </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>x</th>
+                                                        <th>
+                                                            <input
+                                                                type="range"
+                                                                min="0"
+                                                                :max="AvatarImgObj.width - 360"
+                                                                class="range"
+                                                                v-model="AvatarImgOffsetStr.x"
+                                                            />
+                                                        </th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>y</th>
+                                                        <th>
+                                                            <input
+                                                                type="range"
+                                                                min="0"
+                                                                :max="AvatarImgObj.height - 550"
+                                                                class="range"
+                                                                v-model="AvatarImgOffsetStr.y"
+                                                            />
+                                                        </th>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </PopoverPanel>
+            </transition>
+        </Popover>
     </div>
 </template>
 
 <style scoped>
 table[class^='table-'] {
-    margin-left: 50px;
-
     tbody {
         tr {
             th {
