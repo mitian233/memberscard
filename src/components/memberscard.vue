3a3ba3bf4dd97f3666581366831567a7690f9cbd
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2025-01-08 14:28:14 +0800

    feat: update save button style

--- src/components/memberscard.vue
+++ src/components/memberscard.vue
@@ -376,12 +376,7 @@ onBeforeUnmount(() => {
                 </p>
             </div>
         </div>
-        <button
-            class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white"
-            @click="saveAsImage"
-        >
-            保存图片
-        </button>
+        <button class="btn" @click="saveAsImage">保存图片</button>
 
         <table class="table-out">
             <tbody>
